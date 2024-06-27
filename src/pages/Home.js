3aa9e7066210c46be087df8732f1d97051805e68
Hanny <zyhaierciyuan@gmail.com>
2024-06-27 16:47:57 +1000

    Homepage skeleton, waiting for discuss

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,64 +1,26 @@
 import React from "react";
-import "../App.css";
+import { Container, Row, Col, Card } from 'react-bootstrap';
+import "../css/Home.css";
 
 const Home = () => {
     return (
-        <div>
-            <section className='hero'>
-                <div className='hero-text'>
-                    <h1>Lorem ipsum</h1>
-                    <p>
-                    Lorem ipsum dolor sit amet consectetur. Faucibus at
-                    parturient est tincidunt et platea volutpat at. Etiam id
-                    iaculis tincidunt sem scelerisque tincidunt elementum
-                    tellus. Risus vel fermentum faucibus tincidunt.
-                    </p>
-                </div>
-                <img
-                    src='path/to/your/image'
-                    alt='StudyFin'
-                    className='hero-image'
-                />
-            </section>
-
-            <section className='profiles'>
-                <div className='profile'>
-                    <img src='path/to/profile/image' alt='Profile' />
-                    <h2>Lorem ipsum</h2>
-                    <p>Lorem ipsum dolor sit amet consectetur.</p>
-                </div>
-                <div className='profile'>
-                    <img src='path/to/profile/image' alt='Profile' />
-                    <h2>Lorem ipsum</h2>
-                    <p>Lorem ipsum dolor sit amet consectetur.</p>
-                </div>
-                {/* Add more profiles as needed */}
-            </section>
-
-            <section className='feature'>
-                <img
-                    src='path/to/feature/image'
-                    alt='Feature'
-                    className='feature-image'
-                />
-                <div className='feature-text'>
-                    <h1>Lorem ipsum</h1>
-                    <p>
-                    Lorem ipsum dolor sit amet consectetur. Faucibus at
-                    parturient est tincidunt et platea volutpat at. Etiam id
-                    iaculis tincidunt sem scelerisque tincidunt elementum
-                    tellus. Risus vel fermentum faucibus tincidunt.
-                    </p>
-                </div>
-            </section>
-
-            <section className='logos'>
-                <div className='logo'>LOGO</div>
-                <div className='logo'>LOGO</div>
-                <div className='logo'>LOGO</div>
-                <div className='logo'>LOGO</div>
-                {/* Add more logos as needed */}
-            </section>
+        <div className="homepage-container">
+            <Container>
+                <Row md={9} className="card-container">
+                    <Col>
+                        <Card className="home-card"><Card.Img src="/homepage/sort_1.png" alt="Card image" /></Card>
+                    </Col>
+                    <Col>
+                        <Card className="home-card"><Card.Img src="/homepage/sort_2.png" alt="Card image" /></Card>
+                    </Col>
+                    <Col>
+                        <Card className="home-card"><Card.Img src="/homepage/sort_3.png" alt="Card image" /></Card>
+                    </Col>
+                    <Col>
+                        <Card className="home-card"><Card.Img src="/homepage/sort_4.png" alt="Card image" /></Card>
+                    </Col>
+                </Row>
+            </Container>
         </div>
     );
 
