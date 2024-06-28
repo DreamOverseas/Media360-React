4cec633a2889d9c3da41bba68208d1a50efb7839
Hanny <zyhaierciyuan@gmail.com>
2024-06-28 12:41:36 +1000

    Homepage layout done, await for KOL page links

--- src/components/Header.js
+++ src/components/Header.js
@@ -19,16 +19,16 @@ const Header = () => {
           </Navbar>
         </header>
       */}
-      <Navbar bg="light" expand="lg" className='navbar-custom'>
+      <Navbar bg="light" expand="lg" fixed="top" className='navbar-custom'>
         <Nav.Link href='/'><Image className="nav-logo" src="header_logo.png" alt="360 Media" /></Nav.Link>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto">
-            <Nav.Link href="#kolpage">KOL</Nav.Link>
-            <Nav.Link href="#About-Us">Events</Nav.Link>
-            <Nav.Link href="#About-Us">Contact</Nav.Link>
-            <Nav.Link href="#login"><i className="bi bi-person nav-icon"></i></Nav.Link>
-            <Nav.Link href="#cart"><i className="bi bi-cart nav-icon"></i></Nav.Link>
+            <Nav.Link href="/kolpage">KOL</Nav.Link>
+            <Nav.Link href="/About-Us">Events</Nav.Link>
+            <Nav.Link href="/About-Us">Contact</Nav.Link>
+            <Nav.Link href="/login"><i className="bi bi-person nav-icon"></i></Nav.Link>
+            <Nav.Link href="/cart"><i className="bi bi-cart nav-icon"></i></Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Navbar>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,24 +1,45 @@
 import React from "react";
-import { Container, Row, Col, Card } from 'react-bootstrap';
+import { Container, Row, Card } from 'react-bootstrap';
 import "../css/Home.css";
 
 const Home = () => {
     return (
         <div className="homepage-container">
             <Container>
-                <Row md={9} className="card-container">
-                    <Col>
-                        <Card className="home-card"><Card.Img src="/homepage/sort_1.png" alt="Card image" /></Card>
-                    </Col>
-                    <Col>
-                        <Card className="home-card"><Card.Img src="/homepage/sort_2.png" alt="Card image" /></Card>
-                    </Col>
-                    <Col>
-                        <Card className="home-card"><Card.Img src="/homepage/sort_3.png" alt="Card image" /></Card>
-                    </Col>
-                    <Col>
-                        <Card className="home-card"><Card.Img src="/homepage/sort_4.png" alt="Card image" /></Card>
-                    </Col>
+                <Row md={12} className="card-container">
+
+                    <Card className="home-card"><Card.Img src="/homepage/sort_1.png" alt="Card image" />
+                    <Card.ImgOverlay>
+                        <Card.Title className='home-card-text'>Study</Card.Title>
+                        <Card.Text className='home-card-text'>
+                            Perfect for you as you planning to study overseas and seeking for helping hands at the new place!
+                        </Card.Text>
+                    </Card.ImgOverlay></Card>
+
+                    <Card className="home-card"><Card.Img src="/homepage/sort_2.png" alt="Card image" />
+                    <Card.ImgOverlay>
+                        <Card.Title className='home-card-text'>Finance</Card.Title>
+                        <Card.Text className='home-card-text'>
+                            We help you to manage your wealth and multiply your success!
+                        </Card.Text>
+                    </Card.ImgOverlay></Card>
+
+                    <Card className="home-card"><Card.Img src="/homepage/sort_3.png" alt="Card image" />
+                    <Card.ImgOverlay>
+                        <Card.Title className='home-card-text'>Travel</Card.Title>
+                        <Card.Text className='home-card-text'>
+                            Let's go travel with your sudden idea and meet our KOL on the go!
+                        </Card.Text>
+                    </Card.ImgOverlay></Card>
+
+                    <Card className="home-card"><Card.Img src="/homepage/sort_4.png" alt="Card image" />
+                    <Card.ImgOverlay>
+                        <Card.Title className='home-card-text'>Life</Card.Title>
+                        <Card.Text className='home-card-text'>
+                            We care about your life here and feel free to reach out to us!
+                        </Card.Text>
+                    </Card.ImgOverlay></Card>
+
                 </Row>
             </Container>
         </div>
