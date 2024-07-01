eb311ef628d4d112af6fee49738891194530dff6
congcong919 <congcong.zhao0919@gmail.com>
2024-07-01 16:29:39 +1000

    fix form submission bug  and adjust css style of the form

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -4,9 +4,11 @@ import {Tab, Container, Card, Row, Col, Form, Button, Image, Nav} from 'react-bo
 import "../css/Login.css";
 
 const Login = () => {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [confirmed, setConfirmed] = useState("");
+    const [SignInEmail, setSignInEmail] = useState("");
+    const [SignInPassword, setSignInPassword] = useState("");
+    const [SignUpConfirmed, setSignUpConfirmed] = useState("");
+    const [SignUpEmail, setSignUpEmail] =useState("")
+    const [SignUpPassword, setSignUpPassword] =useState("")
     const [submitted, setSubmitted] = useState(false);
     const [k, setK] = useState(true);
     const [activeKey, setActiveKey] = useState('sign-in');
@@ -15,146 +17,157 @@ const Login = () => {
         setK((prev) => (prev === true ? false : true));
     };
 
-    const handleSignin = (event) => {
+    const handleSignIn = (event) => {
         event.preventDefault();
         setSubmitted(true);
-        if (email && password) {
+        if (SignInEmail && SignInPassword) {
             // TODO: Implement login logic here when our server is ready
-            console.log("Logging in with:", email, password);
+            console.log("Logging in with:", SignInEmail, SignInPassword);
+        }
+    };
+
+    const handleSignUp = (event) => {
+        event.preventDefault();
+        setSubmitted(true);
+        if (SignUpEmail && SignUpPassword && SignUpConfirmed) {
+            // TODO: Implement login logic here when our server is ready
+            console.log("Logging in with:", SignInEmail, SignInPassword);
         }
     };
 
     return (
         <Container>
             <Row>
-            <Col md={6}>
-                <Tab.Container defaultActiveKey="sign-in" onSelect={() => loginPicture()}>
-                    <Row>
-                        <Col>
-                            <Nav.Link eventKey="sign-in" 
-                            className={`nav-link-Login ${activeKey === 'sign-in' ? 'active' : ''}`}
-                            onClick={() => setActiveKey('sign-in')}>
-                                Sign in
-                            </Nav.Link>
-                        </Col>
-                        <Col>
-                            <Nav.Link eventKey="sign-up" 
-                            className={`nav-link-Login ${activeKey === 'sign-up' ? 'active' : ''}`}
-                            onClick={() => setActiveKey('sign-up')}>
-                                Sign up
-                            </Nav.Link>
-                        </Col>
-                    </Row>
-                    <Row>
-                        <Tab.Content>
-                            <Tab.Pane eventKey="sign-in">
-                                <Container>
-                                    <Row className="sign-in-table">
-                                        <Card>
-                                            <Card.Body>
-                                                <Form onSubmit={handleSignin}>
-                                                    <Form.Group controlId="userEmail">
-                                                        <Form.Label>Email</Form.Label>
-                                                        <Form.Control
-                                                            type="email"
-                                                            placeholder="Enter email"
-                                                            value={email}
-                                                            onChange={(e) => setEmail(e.target.value)}
-                                                            isInvalid={submitted && !email}
-                                                        />
-                                                        <Form.Control.Feedback type="invalid">
-                                                            Please provide a valid email.
-                                                        </Form.Control.Feedback>
-                                                    </Form.Group>
-
-                                                    <Form.Group controlId="userPassword">
-                                                        <Form.Label>Password</Form.Label>
-                                                        <Form.Control
-                                                            type="password"
-                                                            placeholder="Password"
-                                                            value={password}
-                                                            onChange={(e) => setPassword(e.target.value)}
-                                                            isInvalid={submitted && !password}
-                                                        />
-                                                        <Form.Control.Feedback type="invalid">
-                                                            Password is required.
-                                                        </Form.Control.Feedback>
-                                                    </Form.Group>
-                                                    <div className="form-button-container">
+                <Col md={6}>
+                    <Tab.Container defaultActiveKey="sign-in" onSelect={() => loginPicture()}>
+                        <Row>
+                            <Col>
+                                <Nav.Link eventKey="sign-in" 
+                                className={`nav-link-Login ${activeKey === 'sign-in' ? 'active' : ''}`}
+                                onClick={() => setActiveKey('sign-in')}>
+                                    Sign in
+                                </Nav.Link>
+                            </Col>
+                            <Col>
+                                <Nav.Link eventKey="sign-up" 
+                                className={`nav-link-Login ${activeKey === 'sign-up' ? 'active' : ''}`}
+                                onClick={() => setActiveKey('sign-up')}>
+                                    Sign up
+                                </Nav.Link>
+                            </Col>
+                        </Row>
+                        <Row className="login-table">
+                            <Tab.Content>
+                                <Tab.Pane eventKey="sign-in">
+                                    <Container>
+                                        <Row className="sign-in-table">
+                                            <Card>
+                                                <Card.Body>
+                                                    <Form onSubmit={handleSignIn}>
+                                                        <Form.Group controlId="userEmail">
+                                                            <Form.Label>Email</Form.Label>
+                                                            <Form.Control
+                                                                type="email"
+                                                                placeholder="Enter email"
+                                                                value={SignInEmail}
+                                                                onChange={(e) => setSignInEmail(e.target.value)}
+                                                                isInvalid={submitted && !SignInEmail}
+                                                            />
+                                                            <Form.Control.Feedback type="invalid">
+                                                                Please provide a valid email.
+                                                            </Form.Control.Feedback>
+                                                        </Form.Group>
+                                                        <br></br>
+                                                        <Form.Group controlId="userPassword">
+                                                            <Form.Label>Password</Form.Label>
+                                                            <Form.Control
+                                                                type="password"
+                                                                placeholder="Password"
+                                                                value={SignInPassword}
+                                                                onChange={(e) => setSignInPassword(e.target.value)}
+                                                                isInvalid={submitted && !SignInPassword}
+                                                            />
+                                                            <Form.Control.Feedback type="invalid">
+                                                                Password is required.
+                                                            </Form.Control.Feedback>
+                                                        </Form.Group>
+                                                        <br></br>
                                                         <Button variant="primary" type="submit">
                                                             Sign in
                                                         </Button>
-                                                    </div>
-                                                </Form>
-                                            </Card.Body>
-                                        </Card>
-                                    </Row>
-                                </Container>
-                            </Tab.Pane>
-                            <Tab.Pane eventKey="sign-up">
-                                <Container>
-                                    <Row className="sign-in-table">
-                                        <Card>
-                                            <Card.Body>
-                                                <Form onSubmit={handleSignin}>
-                                                    <Form.Group controlId="userEmail">
-                                                        <Form.Label>Email</Form.Label>
-                                                        <Form.Control
-                                                            type="email"
-                                                            placeholder="Enter email"
-                                                            value={email}
-                                                            onChange={(e) => setEmail(e.target.value)}
-                                                            isInvalid={submitted && !email}
-                                                        />
-                                                        <Form.Control.Feedback type="invalid">
-                                                            Please provide a valid email.
-                                                        </Form.Control.Feedback>
-                                                    </Form.Group>
 
-                                                    <Form.Group controlId="userPassword">
-                                                        <Form.Label>Password</Form.Label>
-                                                        <Form.Control
-                                                            type="password"
-                                                            placeholder="Password"
-                                                            value={password}
-                                                            onChange={(e) => setPassword(e.target.value)}
-                                                            isInvalid={submitted && !password}
-                                                        />
-                                                        <Form.Control.Feedback type="invalid">
-                                                            Password is required.
-                                                        </Form.Control.Feedback>
-                                                    </Form.Group>
-                                                    <Form.Group controlId="confirmedPassword">
-                                                        <Form.Label>Confirmed Password</Form.Label>
-                                                        <Form.Control
-                                                            type="password"
-                                                            placeholder="Password"
-                                                            value={confirmed}
-                                                            onChange={(e) => setConfirmed(e.target.value)}
-                                                            isInvalid={password === confirmed}
-                                                        />
-                                                        <Form.Control.Feedback type="invalid">
-                                                            Two passwords are not the same, please check again.
-                                                        </Form.Control.Feedback>
-                                                    </Form.Group>
-                                                    <div className="form-button-container">
+                                                    </Form>
+                                                </Card.Body>
+                                            </Card>
+                                        </Row>
+                                    </Container>
+                                </Tab.Pane>
+
+                                <Tab.Pane eventKey="sign-up">
+                                    <Container>
+                                        <Row className="sign-up-table">
+                                            <Card>
+                                                <Card.Body>
+                                                    <Form onSubmit={handleSignUp}>
+
+                                                        <Form.Group controlId="userEmail">
+                                                            <Form.Label>Email</Form.Label>
+                                                            <Form.Control
+                                                                type="email"
+                                                                placeholder="Enter email"
+                                                                value={SignUpEmail}
+                                                                onChange={(e) => setSignUpEmail(e.target.value)}
+                                                                isInvalid={submitted && !SignUpEmail}
+                                                            />
+                                                            <Form.Control.Feedback type="invalid">
+                                                                Please provide a valid email.
+                                                            </Form.Control.Feedback>
+                                                        </Form.Group>
+                                                        <br></br>
+                                                        <Form.Group controlId="userPassword">
+                                                            <Form.Label>Password</Form.Label>
+                                                            <Form.Control
+                                                                type="password"
+                                                                placeholder="Password"
+                                                                value={SignUpPassword}
+                                                                onChange={(e) => setSignUpPassword(e.target.value)}
+                                                                isInvalid={submitted && !SignUpPassword}
+                                                            />
+                                                            <Form.Control.Feedback type="invalid">
+                                                                Password is required.
+                                                            </Form.Control.Feedback>
+                                                        </Form.Group>
+                                                        <br></br>
+                                                        <Form.Group controlId="confirmedPassword">
+                                                            <Form.Label>Confirmed Password</Form.Label>
+                                                            <Form.Control
+                                                                type="password"
+                                                                placeholder="Password"
+                                                                value={SignUpConfirmed}
+                                                                onChange={(e) => setSignUpConfirmed(e.target.value)}
+                                                                isInvalid={submitted && !SignUpPassword && !SignUpConfirmed && SignUpPassword == SignUpConfirmed}
+                                                            />
+                                                            <Form.Control.Feedback type="invalid">
+                                                                Two passwords are not the same, please check again.
+                                                            </Form.Control.Feedback>
+                                                        </Form.Group>
+                                                        <br></br>
                                                         <Button variant="primary" type="submit">
                                                             Sign up
                                                         </Button>
-                                                    </div>
-                                                </Form>
-                                            </Card.Body>
-                                        </Card>
-                                    </Row>
-                                </Container>
-                            </Tab.Pane>
-                        </Tab.Content>
-                    </Row>
-                </Tab.Container>
-            </Col>
-            <Col md={6}>
-                <Image src={ k ? "https://placehold.co/650x650" : "https://placehold.co/750x750"} fluid/>
-            </Col>
+                                                    </Form>
+                                                </Card.Body>
+                                            </Card>
+                                        </Row>
+                                    </Container>
+                                </Tab.Pane>
+                            </Tab.Content>
+                        </Row>
+                    </Tab.Container>
+                </Col>
+                <Col md={6}>
+                    <Image src={ k ? "https://placehold.co/650x650" : "https://placehold.co/750x750"} fluid/>
+                </Col>
             </Row>
         </Container>
     );
