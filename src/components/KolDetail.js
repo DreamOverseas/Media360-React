7559058cc0fb8bcf087c416643bd51004bcc5d7a
congcong919 <congcong.zhao0919@gmail.com>
2024-07-10 16:59:53 +1000

    modify kol detail page version 1.0

--- src/components/KolDetail.js
+++ src/components/KolDetail.js
@@ -13,7 +13,7 @@ const KolDetail = () => {
 
   useEffect(() => {
     axios
-      .get(`http://192.168.20.91:1337/api/kols/${id}?populate=products`)
+      .get(`http://192.168.20.14:1337/api/kols/${id}?populate=products`)
       .then(response => {
         if (response.data && response.data.data) {
           setKol(response.data.data);
@@ -52,6 +52,9 @@ const KolDetail = () => {
   };
 
   const renderDescription = (description) => {
+    if (!Array.isArray(description)) {
+      return <span>{description}</span>;
+    }
     return description.map((desc, index) => (
       <React.Fragment key={index}>
         {desc.children.map((child, idx) => (
@@ -62,85 +65,100 @@ const KolDetail = () => {
   };
 
   return (
-    <Container className='kol-detail-container'>
-      <Row>
-        <Col md={12} className='kol-header'>
-          {KolImage && KolImage.data ? (
-            <Image src={KolImage.data.attributes.url} alt={Name} fluid />
+    <div>
+      <section className="kol-detail-background-image-container">
+        <h1 className = "kol-detail-banner-h1"><b>Our People</b></h1>
+      </section>
+      <br />
+      <section>
+        <Container fluid>
+          <Row className="kol-detail-section">
+              <Col>
+                <Container>
+                  <Row>
+                    <h1>{Name}</h1>
+                    <h2>{Title}</h2>
+                  </Row>
+                  <Row>
+                      <p>
+                        {Description ? renderDescription(Description) : "No description available"}
+                      </p>
+                  </Row> 
+                  <Row>
+                    <Button variant='primary'>Contact Now</Button>
+                  </Row>
+                </Container>
+              </Col>
+              <Col md={5}>
+                {KolImage && KolImage.data ? (<Image src={KolImage.data.attributes.url} alt={Name} fluid />) : 
+                (<Image src='https://placehold.co/500x600' alt='Placeholder' fluid />)}
+              </Col>
+          </Row>
+        </Container>
+      </section>
+      <Container className='kol-detail-container'>
+        <Row>
+          <Col md={12}>
+            <h3>Product Recommendation</h3>
+          </Col>
+          {products.data.length > 0 ? (
+            products.data.map(product => (
+              <Col key={product.id} sm={12} md={6} lg={4}>
+                <Card className='product-card' onClick={() => handleProductClick(product)}>
+                  {product.attributes.Image && product.attributes.Image.data ? (
+                    <Card.Img
+                      variant='top'
+                      src={product.attributes.Image.data.attributes.url}
+                    />
+                  ) : (
+                    <Card.Img variant='top' src='https://placehold.co/300x300' />
+                  )}
+                  <Card.Body>
+                    <Card.Title>{product.attributes.Name}</Card.Title>
+                    <Card.Text>Price: ${product.attributes.Price}</Card.Text>
+                    <Card.Text>
+                      {product.attributes.Description
+                        ? renderDescription(product.attributes.Description)
+                        : "No description available"}
+                    </Card.Text>
+                  </Card.Body>
+                </Card>
+              </Col>
+            ))
           ) : (
-            <Image src='https://placehold.co/300x300' alt='Placeholder' fluid />
+            <p>No products available</p>
           )}
-          <h1>{Name}</h1>
-          <h2>{Title}</h2>
-          <p>{Description ? renderDescription(Description) : "No description available"}</p>
-          <Button variant='primary'>Contact Now</Button>
-          <div className='social-icons'>
-            <i className='fab fa-twitter'></i>
-            <i className='fab fa-instagram'></i>
-            <i className='fab fa-facebook'></i>
-          </div>
-        </Col>
-      </Row>
-      <Row>
-        <Col md={12}>
-          <h3>Product Recommendation</h3>
-        </Col>
-        {products.data.length > 0 ? (
-          products.data.map(product => (
-            <Col key={product.id} sm={12} md={6} lg={4}>
-              <Card className='product-card' onClick={() => handleProductClick(product)}>
-                {product.attributes.Image && product.attributes.Image.data ? (
-                  <Card.Img
-                    variant='top'
-                    src={product.attributes.Image.data.attributes.url}
-                  />
-                ) : (
-                  <Card.Img variant='top' src='https://placehold.co/300x300' />
-                )}
-                <Card.Body>
-                  <Card.Title>{product.attributes.Name}</Card.Title>
-                  <Card.Text>Price: ${product.attributes.Price}</Card.Text>
-                  <Card.Text>
-                    {product.attributes.Description
-                      ? renderDescription(product.attributes.Description)
-                      : "No description available"}
-                  </Card.Text>
-                </Card.Body>
-              </Card>
-            </Col>
-          ))
-        ) : (
-          <p>No products available</p>
-        )}
-      </Row>
+        </Row>
 
-      {/* Product Detail Modal */}
-      {selectedProduct && (
-        <Modal show={showModal} onHide={handleCloseModal}>
-          <Modal.Header closeButton>
-            <Modal.Title>{selectedProduct.attributes.Name}</Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
-            {selectedProduct.attributes.Image && selectedProduct.attributes.Image.data ? (
-              <Image src={selectedProduct.attributes.Image.data.attributes.url} alt={selectedProduct.attributes.Name} fluid />
-            ) : (
-              <Image src='https://placehold.co/300x300' alt='Placeholder' fluid />
-            )}
-            <p>Price: ${selectedProduct.attributes.Price}</p>
-            <p>
-              {selectedProduct.attributes.Description
-                ? renderDescription(selectedProduct.attributes.Description)
-                : "No description available"}
-            </p>
-          </Modal.Body>
-          <Modal.Footer>
-            <Button variant='secondary' onClick={handleCloseModal}>
-              Close
-            </Button>
-          </Modal.Footer>
-        </Modal>
-      )}
-    </Container>
+        {/* Product Detail Modal */}
+        {selectedProduct && (
+          <Modal show={showModal} onHide={handleCloseModal}>
+            <Modal.Header closeButton>
+              <Modal.Title>{selectedProduct.attributes.Name}</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              {selectedProduct.attributes.Image && selectedProduct.attributes.Image.data ? (
+                <Image src={selectedProduct.attributes.Image.data.attributes.url} alt={selectedProduct.attributes.Name} fluid />
+              ) : (
+                <Image src='https://placehold.co/300x300' alt='Placeholder' fluid />
+              )}
+              <p>Price: ${selectedProduct.attributes.Price}</p>
+              <p>
+                {selectedProduct.attributes.Description
+                  ? renderDescription(selectedProduct.attributes.Description)
+                  : "No description available"}
+              </p>
+            </Modal.Body>
+            <Modal.Footer>
+              <Button variant='secondary' onClick={handleCloseModal}>
+                Close
+              </Button>
+            </Modal.Footer>
+          </Modal>
+        )}
+      </Container>
+    </div>
+    
   );
 };
 
