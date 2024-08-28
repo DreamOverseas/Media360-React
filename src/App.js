63b7155231a3035f4df0ac8ce6df0ee6ac9ec1f0
Evan5683 <104410745+Evan5683@users.noreply.github.com>
2024-08-28 13:59:06 +1000

    update homepage
    update homepage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
+import "bootstrap-icons/font/bootstrap-icons.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 import React, { useEffect, useState } from "react";
+import { useMediaQuery } from "react-responsive";
 import { Route, Routes, useLocation } from "react-router-dom";
-import { useMediaQuery } from 'react-responsive';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap-icons/font/bootstrap-icons.css';
-import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import "slick-carousel/slick/slick.css";
 import "./App.css";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
@@ -13,30 +13,30 @@ import KolPage from "./components/KolPage";
 import ProductDetail from "./components/ProductDetail";
 import ProductPage from "./components/ProductPage";
 import Contact from "./pages/Contact";
-import Home from "./pages/Home";
-import MobileHome from "./pages/MobileHome";
-import Login from "./pages/Login";
-import ShoppingCart from "./pages/ShoppingCart";
 import Events from "./pages/Events";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import MobileHome from "./pages/MobileHome";
 import Profile from "./pages/Profile";
+import ShoppingCart from "./pages/ShoppingCart";
 
 function App() {
   // If n only if homepage, the topmargin is 0
   const location = useLocation();
   const [footer, setFooter] = useState();
   useEffect(() => {
-    if (location.pathname === '/') {
-      document.body.style.marginTop = '0';
+    if (location.pathname === "/") {
+      document.body.style.marginTop = "0";
       // eslint-disable-next-line react-hooks/exhaustive-deps
       setFooter(<></>);
     } else {
-      document.body.style.marginTop = '67px'; // Reserved margin for fix-top Navbar
+      document.body.style.marginTop = "67px"; // Reserved margin for fix-top Navbar
       setFooter(<Footer />);
     }
   }, [location]);
 
   // Check if is on desktop
-  const isDesktop = useMediaQuery({ query: '(min-width: 768px)' });
+  const isDesktop = useMediaQuery({ query: "(min-width: 768px)" });
 
   return (
     <div className='App'>
---
+++ src/components/Advertisement.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { Carousel, Image } from "react-bootstrap";
+import "../css/Advertisement.css";
+
+const BACKEND_HOST = process.env.REACT_APP_STRAPI_HOST;
+
+const Advertisement = ({ ads }) => {
+  return (
+    <Carousel className='ads-carousel' interval={3000}>
+      {ads.map((advertisement, index) => (
+        <Carousel.Item key={index}>
+          <Image
+            src={
+              advertisement.attributes.Adimage?.data?.[0]?.attributes?.url
+                ? `${BACKEND_HOST}${advertisement.attributes.Adimage.data[0].attributes.url}`
+                : "https://placehold.co/900x400"
+            }
+            className='d-block w-100 ad-image'
+            alt={advertisement.attributes.Name || "Advertisement"}
+          />
+          <Carousel.Caption>
+            <h3>{advertisement.attributes.Name}</h3>
+          </Carousel.Caption>
+        </Carousel.Item>
+      ))}
+    </Carousel>
+  );
+};
+
+export default Advertisement;
