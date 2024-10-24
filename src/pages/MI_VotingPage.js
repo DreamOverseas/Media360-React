e998d47743125787ca0a778a7cd0e8ff6a0b17a0
Hanny <zyhaierciyuan@gmail.com>
2024-10-24 15:48:29 +1100

    Adding a Control panel that allows regular refreshing

--- src/pages/MI_VotingPage.js
+++ src/pages/MI_VotingPage.js
@@ -1,13 +1,49 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Button, Form } from 'react-bootstrap';
 
 const MIVoting = () => {
+    const [refreshInterval, setRefreshInterval] = useState(5000);
+    const [key, setKey] = useState(0);
+
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            setKey(prevKey => prevKey + 1);
+        }, refreshInterval);
+
+        return () => clearInterval(intervalId);
+    }, [refreshInterval]);
+
+    const handleRefreshIntervalChange = (event) => {
+        setRefreshInterval(Number(event.target.value));
+    };
+
     return (
         <div style={styles.votingPage}>
-            <iframe 
+            <iframe
                 src="https://www.missinternational.world/Index/votenow.html"
                 title="Embedded Page"
+                key={key}
                 style={styles.miWebsite}
             ></iframe>
+
+            <div style={styles.refreshControlPanel}>
+                <Form.Group controlId="refreshSpeed">
+                    <Form.Label>刷新间隔：</Form.Label>
+                    <Form.Control
+                        as="select"
+                        value={refreshInterval}
+                        onChange={handleRefreshIntervalChange}
+                    >
+                        <option value={2000}>每2s</option>
+                        <option value={5000}>每5s</option>
+                        <option value={10000}>每10s</option>
+                        <option value={30000}>每30s</option>
+                    </Form.Control>
+                </Form.Group>
+                {/* <Button onClick={}>
+                    现在刷新
+                </Button> */}
+            </div>
         </div>
     );
 };
@@ -16,7 +52,7 @@ const styles = {
     votingPage: {
         height: '100vh',
         overflow: 'hidden',
-        backgroundImage: 'url("/homepage/bg_clear.png")',
+        backgroundImage: 'url("/mi_bg.png")',
         backgroundSize: 'cover',
         backgroundPosition: 'center',
         display: 'flex',
@@ -28,9 +64,18 @@ const styles = {
         height: '100vh',
         overflow: 'auto',
         border: 'none',
-        borderRadius: '10px', 
+        borderRadius: '10px',
         boxShadow: '0px 0px 10px rgba(0, 0, 0, 0.5)',
         display: 'block',
+    },
+    refreshControlPanel: {
+        position: 'fixed',
+        bottom: '20px',
+        right: '20px',
+        backgroundColor: 'rgba(255, 255, 255, 0.8)',
+        padding: '10px',
+        borderRadius: '8px',
+        boxShadow: '0px 0px 5px rgba(0, 0, 0, 0.3)',
     }
 };
 
