4bc3b75a8e29a2481b0f3eac3c4bc22bf2f2991c
congcong919 <110518940+congcong919@users.noreply.github.com>
2025-02-12 02:14:18 +1100

    page css layout optimization and add share link

--- src/components/ProductDetail.js
+++ src/components/ProductDetail.js
@@ -50,21 +50,23 @@ const ProductDetail = () => {
           className="mb-3"
         >
           {data.map((item) => (
-            <Tab eventKey={item.id} title={item.Name} key={item.id}>
-              <Row className="align-items-center">
-                <Col md={6}>
+            <Tab eventKey={item.id} title={item.Name_zh} key={item.id}>
+              <Row className="person-detail">
+                <Col md={4}>
                   <Image src={`${BACKEND_HOST}${item.Image[0].url}`} alt={item.Name} fluid />
                 </Col>
-                <Col md={6}>
-                {item.Bio_zh ? (
-                    <div className="markdown-content">
-                      <ReactMarkdown rehypePlugins={[rehypeRaw]}>{item.Bio_zh}</ReactMarkdown>
-                    </div>
-                  ) : (
-                    <div className="ck-content" dangerouslySetInnerHTML={{ __html: item.Bio_zh}} />
-                )}
+                <Col md={8}>
+                  <p>{item.Name_zh}</p>
+                  {item.Bio_zh ? (
+                        <ReactMarkdown rehypePlugins={[rehypeRaw]}>{item.Bio_zh}</ReactMarkdown>
+                    ) : (
+                      <div className="ck-content" dangerouslySetInnerHTML={{ __html: item.Bio_zh}} />
+                  )}
+                    <Link to={`/kol/${item.id}`} className="person-related-btn">查看更多</Link>
+                    <Link to={`/`} className="person-related-btn">更新信息</Link>
                 </Col>
               </Row>
+              
             </Tab>
           ))}
         </Tabs>
@@ -77,7 +79,7 @@ const ProductDetail = () => {
   const DescriptionAccordion = ({ id, accordion_name, content}) => {
     const [activeAccordion, setActiveAccordion] = useState(null);
     const toggleAccordion = () => {
-      setActiveAccordion(id);
+      setActiveAccordion(activeAccordion === id ? null : id);
     };
 
     return (
@@ -104,7 +106,7 @@ const ProductDetail = () => {
   const FounderAccordion = ({ id, accordion_name, content}) => {
     const [activeAccordion, setActiveAccordion] = useState(null);
     const toggleAccordion = () => {
-      setActiveAccordion(id);
+      setActiveAccordion(activeAccordion === id ? null : id);
     };
 
     return (
@@ -125,7 +127,7 @@ const ProductDetail = () => {
   const KolAccordion = ({ id, accordion_name, content}) => {
     const [activeAccordion, setActiveAccordion] = useState(null);
     const toggleAccordion = () => {
-      setActiveAccordion(id);
+      setActiveAccordion(activeAccordion === id ? null : id);
     };
 
     return (
@@ -146,7 +148,7 @@ const ProductDetail = () => {
   const SpokesAccordion = ({ id, accordion_name, content}) => {
     const [activeAccordion, setActiveAccordion] = useState(null);
     const toggleAccordion = () => {
-      setActiveAccordion(id);
+      setActiveAccordion(activeAccordion === id ? null : id);
     };
 
     return (
@@ -165,14 +167,14 @@ const ProductDetail = () => {
   };
 
 
-  const handleLoginModalOpen = () => {
-    setShowLoginModal(true);
-    setCartModal(false);
-  };
+  // const handleLoginModalOpen = () => {
+  //   setShowLoginModal(true);
+  //   setCartModal(false);
+  // };
 
-  const handleLoginModalClose = () => setShowLoginModal(false);
+  // const handleLoginModalClose = () => setShowLoginModal(false);
 
-  const handleCloseCartModal = () => setCartModal(false);
+  // const handleCloseCartModal = () => setCartModal(false);
 
   const handleDecrement = () => {
     if (quantity > 1) {
@@ -186,46 +188,46 @@ const ProductDetail = () => {
     }
   };
 
-  const handleAddToCart = async () => {
-    if (user && Cookies.get("token")) {
-      try {
-        const user_detail = await axios.get(`${BACKEND_HOST}/api/users/${user.id}?populate[cart]=*`);
-        const cartid = user_detail.data.cart.id;
-        const cart_items = await axios.get(`${BACKEND_HOST}/api/carts/${cartid}?populate[cart_items][populate][product]=*`);
-        const cart_items_data = cart_items.data.cart_items;
+  // const handleAddToCart = async () => {
+  //   if (user && Cookies.get("token")) {
+  //     try {
+  //       const user_detail = await axios.get(`${BACKEND_HOST}/api/users/${user.id}?populate[cart]=*`);
+  //       const cartid = user_detail.data.cart.id;
+  //       const cart_items = await axios.get(`${BACKEND_HOST}/api/carts/${cartid}?populate[cart_items][populate][product]=*`);
+  //       const cart_items_data = cart_items.data.cart_items;
 
-        const existingItem = cart_items_data.find(item => item.product.id === product.id);
+  //       const existingItem = cart_items_data.find(item => item.product.id === product.id);
 
-        if (existingItem) {
-          await axios.put(
-            `${BACKEND_HOST}/api/cart-items/${existingItem.id}`,
-            { data: { Number: existingItem.attributes.Number + quantity } },
-            { headers: { Authorization: `Bearer ${Cookies.get("token")}`, "Content-Type": "application/json" } }
-          );
-        } else {
-          await axios.post(
-            `${BACKEND_HOST}/api/cart-items`,
-            { data: { Number: quantity, product: product.id } },
-            { headers: { Authorization: `Bearer ${Cookies.get("token")}`, "Content-Type": "application/json" } }
-          );
-        }
+  //       if (existingItem) {
+  //         await axios.put(
+  //           `${BACKEND_HOST}/api/cart-items/${existingItem.id}`,
+  //           { data: { Number: existingItem.attributes.Number + quantity } },
+  //           { headers: { Authorization: `Bearer ${Cookies.get("token")}`, "Content-Type": "application/json" } }
+  //         );
+  //       } else {
+  //         await axios.post(
+  //           `${BACKEND_HOST}/api/cart-items`,
+  //           { data: { Number: quantity, product: product.id } },
+  //           { headers: { Authorization: `Bearer ${Cookies.get("token")}`, "Content-Type": "application/json" } }
+  //         );
+  //       }
 
-        console.log("Item added to cart successfully.");
-      } catch (error) {
-        console.error("Failed to add to cart:", error);
-      }
-    } else {
-      setCartModal(true);
-    }
-  };
+  //       console.log("Item added to cart successfully.");
+  //     } catch (error) {
+  //       console.error("Failed to add to cart:", error);
+  //     }
+  //   } else {
+  //     setCartModal(true);
+  //   }
+  // };
 
-  const handlePurchase = () => {
-    setShowModal(true);
-  };
+  // const handlePurchase = () => {
+  //   setShowModal(true);
+  // };
 
-  const handleCloseModal = () => {
-    setShowModal(false);
-  };
+  // const handleCloseModal = () => {
+  //   setShowModal(false);
+  // };
 
 
 
@@ -268,6 +270,24 @@ const ProductDetail = () => {
       setSpokesperson(people.filter(person => person.Role === "Spokesperson"));
     }
   }, [people]);
+
+
+  const handleShare = () => {
+    const shareData = {
+      title: "页面分享",
+      text: document.title,
+      url: window.location.href,
+    };
+
+    if (navigator.share) {
+      navigator
+        .share(shareData)
+        .then(() => console.log("分享成功！"))
+        .catch((error) => console.error("分享失败:", error));
+    } else {
+      alert("当前浏览器不支持分享功能，请手动复制链接分享。");
+    }
+  };
 
   if (!product) {
     return (
@@ -383,8 +403,7 @@ const ProductDetail = () => {
                     content={spokesperson}
                   />
                 </Row>
-
-                <Row>
+                {/* <Row>
                   {(Price !== 0 || 1) && Available ? (
                     <>
                       <Col>
@@ -397,20 +416,25 @@ const ProductDetail = () => {
                   ) : (
                     <Button className='add-to-cart' onClick={handlePurchase}>{t("enquireNow")}</Button>
                   )}
+                </Row> */}
+                <Row>
+                  <a href="#" onClick={handleShare} className="social-sharing__link" title="分享">
+                    <i class="icon-share">
+                      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 576" width="20" height="20">
+                        <path d="M400 255.4l0-15.4 0-32c0-8.8-7.2-16-16-16l-32 0-16 0-46.5 0c-50.9 0-93.9 33.5-108.3 79.6c-3.3-9.4-5.2-19.8-5.2-31.6c0-61.9 50.1-112 112-112l48 0 16 0 32 0c8.8 0 16-7.2 16-16l0-32 0-15.4L506 160 400 255.4zM336 240l16 0 0 48c0 17.7 14.3 32 32 32l3.7 0c7.9 0 15.5-2.9 21.4-8.2l139-125.1c7.6-6.8 11.9-16.5 11.9-26.7s-4.3-19.9-11.9-26.7L409.9 8.9C403.5 3.2 395.3 0 386.7 0C367.5 0 352 15.5 352 34.7L352 80l-16 0-32 0-16 0c-88.4 0-160 71.6-160 160c0 60.4 34.6 99.1 63.9 120.9c5.9 4.4 11.5 8.1 16.7 11.2c4.4 2.7 8.5 4.9 11.9 6.6c3.4 1.7 6.2 3 8.2 3.9c2.2 1 4.6 1.4 7.1 1.4l2.5 0c9.8 0 17.8-8 17.8-17.8c0-7.8-5.3-14.7-11.6-19.5c0 0 0 0 0 0c-.4-.3-.7-.5-1.1-.8c-1.7-1.1-3.4-2.5-5-4.1c-.8-.8-1.7-1.6-2.5-2.6s-1.6-1.9-2.4-2.9c-1.8-2.5-3.5-5.3-5-8.5c-2.6-6-4.3-13.3-4.3-22.4c0-36.1 29.3-65.5 65.5-65.5l14.5 0 32 0zM72 32C32.2 32 0 64.2 0 104L0 440c0 39.8 32.2 72 72 72l336 0c39.8 0 72-32.2 72-72l0-64c0-13.3-10.7-24-24-24s-24 10.7-24 24l0 64c0 13.3-10.7 24-24 24L72 464c-13.3 0-24-10.7-24-24l0-336c0-13.3 10.7-24 24-24l64 0c13.3 0 24-10.7 24-24s-10.7-24-24-24L72 32z"/>
+                      </svg>
+                    </i>
+                    <span className="share-title">分享链接</span>
+                  </a>
                 </Row>
-
-
-
+          
               </Container>
-
 
             </Col>
 
-
-
           </Row>
         </Container>
-        <Modal show={showModal} onHide={handleCloseModal}>
+        {/* <Modal show={showModal} onHide={handleCloseModal}>
           <Modal.Header closeButton>
             <Modal.Title>{Name}</Modal.Title>
           </Modal.Header>
@@ -447,7 +471,7 @@ const ProductDetail = () => {
           </Modal.Footer>
         </Modal>
 
-        <LoginModal show={showLoginModal} handleClose={handleLoginModalClose} />
+        <LoginModal show={showLoginModal} handleClose={handleLoginModalClose} /> */}
       </section>
       <br />
       <br />
