4a5424942cc9147b8427cf81ad9c1bb6e7aaf8c9
congcong919 <110518940+congcong919@users.noreply.github.com>
2025-03-05 11:13:16 +1100

    connect with the ai chatbot and update version the react

--- src/components/FloatingHomeButton.js
+++ src/components/FloatingHomeButton.js
@@ -1,7 +1,15 @@
 import React from "react";
 import { BsHouse, BsArrowLeft, BsArrowRight } from "react-icons/bs";
 import { useNavigate } from "react-router-dom";
+import CuteChatbot from "@dreamoverseas/cute-chatbot";
 import "../css/FloatingHomeButton.css";
+
+
+
+const API_URL = process.env.REACT_APP_OPENAI_API_URL
+const ASST_ID = process.env.REACT_APP_OPENAI_ASST_ID
+const API_KEY = process.env.REACT_APP_OPENAI_API_KEY
+const GOOGLE_API = process.env.REACT_APP_GOOGLE_API
 
 const FloatingHomeButton = () => {
   const navigate = useNavigate();
@@ -20,6 +28,15 @@ const FloatingHomeButton = () => {
 
       <div className='floating-button' onClick={() => navigate(+1)}>
         <BsArrowRight size={24} />
+      </div>
+
+      <div>
+        <CuteChatbot
+          openai_api_url={`${API_URL}`}
+          openai_asst_id={`${ASST_ID}`}
+          openai_api_key={`${API_KEY}`}
+          google_api_key={`${GOOGLE_API}`}
+        />
       </div>
     </div>
   );
