dc793cafd242a8e30ca62cff1e308f15138155d1
congcong919 <110518940+congcong919@users.noreply.github.com>
2025-06-23 09:49:16 +1000

    adjust layout on product detail page

--- src/components/ProductDetail.jsx
+++ src/components/ProductDetail.jsx
@@ -737,12 +737,9 @@ useEffect(() => {
                     </Link>
                   )}
                 </Row>
- 
-                {onDesktop ? (
-                  <></>
-                ) : (
-                  <>
-                    <AccordionItem
+
+                <>
+                  <AccordionItem
                       idx="detail-accordion"
                       header={DetailHeading}
                       detail={Detail}
@@ -760,9 +757,12 @@ useEffect(() => {
                     ):(
                       <></>
                     )}
-
-                    
-
+                </>
+ 
+                {onDesktop ? (
+                  <></>
+                ) : (
+                  <>
                     <Row>
                       <h4>查看相关信息</h4>
                       <Row>
@@ -848,20 +848,10 @@ useEffect(() => {
                         <span className='share-title'>分享此产品</span>
                       </button>
                     </Row> */}
-
-                    
                   </>
-
                   
                 )}
 
-              
-                <AccordionItem
-                  idx="detail-accordion"
-                  header={DetailHeading}
-                  detail={Detail}
-                  defaultOpen={false}
-                />
 
               </Container>
             </Col>
