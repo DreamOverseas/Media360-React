f781c95a3a971a0c826756d53fa2a7601810776f
JeRRyChenwen <88228655+JeRRyChenwen@users.noreply.github.com>
2025-06-23 18:52:02 +1000

    dynamic varibale

--- src/components/JoinUsButton.jsx
+++ src/components/JoinUsButton.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
 import { Button } from 'react-bootstrap';
+import { useParams } from 'react-router-dom';
 
-const JoinUsButton = (props) => (
-  <Button className="product-detail-funtion-btn" {...props}>
-    成为合作伙伴
-  </Button>
-);
+const productTitleMap = {
+  Studyfin: "留学中介",
+  "罗塞尼斯半岛度假村": "旅游中介",
+  "AI美甲": "加盟商",
+};
+
+const JoinUsButton = (props) => {
+  const { productName } = useParams();
+  const role = productTitleMap[productName] || "合作伙伴";
+
+  return (
+    <Button className="product-detail-funtion-btn" {...props}>
+      成为{role}
+    </Button>
+  );
+};
 
 export default JoinUsButton;
