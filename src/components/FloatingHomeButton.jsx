d7b51fb90acaec6a9aaa552e063b72c3f6c003bb
Hanny658 <hannyanhai@gmail.com>
2025-07-29 13:26:22 +1000

    Compatible with no backendURL provided

--- src/components/FloatingHomeButton.jsx
+++ src/components/FloatingHomeButton.jsx
@@ -2,11 +2,16 @@ import React from "react";
 import { BsHouse, BsArrowLeft, BsArrowRight } from "react-icons/bs";
 import { useNavigate } from "react-router-dom";
 import CuteChatbot from "@dreamoverseas/cute-chatbot";
-import { Row, Col} from "react-bootstrap";
+import { Row, Col } from "react-bootstrap";
 import "../css/FloatingHomeButton.css";
 
 const GOOGLE_API = import.meta.env.VITE_GOOGLE_API
 const CHAT_API = import.meta.env.VITE_CHAT_URL
+const ASST_ID = import.meta.env.VITE_OPENAI_ASST_ID
+const API_KEY = import.meta.env.VITE_OPENAI_API_KEY
+const API_URL = import.meta.env.VITE_OPENAI_API_URL
+
+const CHATBOT_NAME = "DO Bot"
 
 const FloatingHomeButton = () => {
   const navigate = useNavigate();
@@ -31,15 +36,25 @@ const FloatingHomeButton = () => {
         </div>
       </Col>
       <Col>
-      <div className="chatbot">
-      <CuteChatbot
-          style={{ zIndex: 101, bottom:'75px !important' }}
-          nickname='DoBot'
-          backend_url={CHAT_API}
-          google_api_key={`${GOOGLE_API}`}
-        />
-      </div>
-        
+        <div className="chatbot">
+          {CHAT_API ?
+            <CuteChatbot
+              style={{ zIndex: 101, bottom: '75px !important' }}
+              nickname={CHATBOT_NAME}
+              backend_url={CHAT_API}
+              google_api_key={`${GOOGLE_API}`}
+            />
+            :
+            <CuteChatbot
+              style={{ zIndex: 101, bottom: '75px !important' }}
+              nickname={CHATBOT_NAME}
+              openai_api_url={`${API_URL}`}
+              openai_asst_id={`${ASST_ID}`}
+              openai_api_key={`${API_KEY}`}
+              google_api_key={`${GOOGLE_API}`}
+            />
+          }
+        </div>
       </Col>
     </Row>
   );
