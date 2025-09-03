460f3a60d263d97d92b75be5755b3ebc93eb78c1
Evan5683 <104410745+Evan5683@users.noreply.github.com>
2025-09-03 19:33:13 +1000

    Revert "Remove unnecessary influencer_profile population param"
    This reverts commit f7249a2eae5747bd383a5afdc89ea941f4c6b704.

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -40,6 +40,7 @@ const AuthProvider = ({ children }) => {
       const response = await axios.get(`${BACKEND_HOST}/api/users/me`, {
         params: {
           "populate[avatar]": "*",
+          "populate[influencer_profile][populate]": "*",
         },
         headers: {
           Authorization: `Bearer ${token}`,
