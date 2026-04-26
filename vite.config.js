b0dfcb0dfcd3863f19c2c9e17181f1402cef9137
xing142857 <xing142857@gmail.com>
2026-04-26 15:16:21 +1000

    No Cache

--- vite.config.js
+++ vite.config.js
@@ -8,4 +8,13 @@ export default defineConfig({
   server: {
     port: 3000,
   },
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: '[name].js',
+        chunkFileNames: '[name].js',
+        assetFileNames: '[name].[ext]',
+      },
+    },
+  },
 });
